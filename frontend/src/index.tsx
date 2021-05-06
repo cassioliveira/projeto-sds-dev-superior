ac5becbfd7ae886cbf131444a025c7c512dde9c9
Cassio Oliveira <cassio@cassioliveira.com.br>
2021-05-06 10:59:25 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
+
+import 'assets/css/styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
